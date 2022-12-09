89c7971fd5f1b5f465c1db88f61fbf8941bc7eb6
PrinceJetro <adegbuyijephthah@gmail.com>
2022-12-09 04:51:40 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+int main(void)
+{
+int lw = 'a';
+int up = 'A';
+while (lw <= "z")
+{
+putchar(lw);
+lw = lw +1
+}
+while (up <= "Z")
+{
+putchar(lw);
+up = up +1
+}
+putchar("\n");
+return (0);
+}
+
