ea3b5851e49527cb67b23b9a928d672e8eb7c415
PrinceJetro <adegbuyijephthah@gmail.com>
2022-12-13 12:05:06 -0800

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet - entry
- * return 0
+ * print_alphabet - entry point
+ * Return: 0
  */
 
 void print_alphabet(void)
 {
 char c = 'a';
+
 while (c <= 'z')
 {
 _putchar(c);
 c++;
+
 }
-putchar('\n');
+_putchar('\n');
 }
