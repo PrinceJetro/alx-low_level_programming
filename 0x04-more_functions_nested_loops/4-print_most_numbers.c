43d8422e7d02bbedb6fe801a9cf2407f464943f6
PrinceJetro <adegbuyijephthah@gmail.com>
2022-12-15 03:49:03 -0800

    more debugging

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,35 @@
 #include "main.h"
 
+
+
 /**
- * print_numbers - prints number
- */
+ *
+ *  * print_most_numbers - prints number
+ *
+ *   */
+
+
 
 void print_most_numbers(void)
+
 {
-int a = 48;
-while (a < 58)
-{
-_putchar(a);
-a++;
-if (a == 50 || a == 52)
-{
-a++;
-}
-}
-_putchar('\n');
-}
+
+		int i;
+
+
+
+			for (i = 48; i < 58; i++)
+
+					{
+
+								if (i != 50 && i != 52)
+
+											{
+
+															_putchar(i);
+
+																	}
+
+									}
+
+
