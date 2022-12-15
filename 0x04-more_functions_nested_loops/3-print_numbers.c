1ac0b6f41aa08c1c3f090dbf3c07bfb23b7cdb70
PrinceJetro <adegbuyijephthah@gmail.com>
2022-12-15 00:28:02 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 nums
+ * @a: first param
+ * @b: second param
+ * Return: something
+ */
+
+void print_numbers(void)
+{
+int a = 48;
+while (a < 58)
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+
+}
