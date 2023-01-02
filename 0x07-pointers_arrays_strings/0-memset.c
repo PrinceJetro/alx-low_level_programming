467577613eb1877aa2c7da05c4e3e7f8ce363174
PrinceJetro <adegbuyijephthah@gmail.com>
2023-01-02 03:14:06 -0800

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -24,18 +24,18 @@ char *_memset(char *s, char b, unsigned int n)
 
 {
 
-		unsigned int counter;
+unsigned int counter;
 
 
 
-			for (counter = 0; counter < n; counter++)
+for (counter = 0; counter < n; counter++)
 
-					{
+{
 
-								s[counter] = b;
+s[counter] = b;
 
 									}
 
-				return (s);
+return (s);
 
 }
